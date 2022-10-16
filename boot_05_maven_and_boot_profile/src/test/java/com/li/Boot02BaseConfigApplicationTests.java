8e23198910140dfe04b73b4d04f8c8ddcd2c87a6
Your Name <930614591@qq.com>
2022-10-16 13:15:55 +0800

    bootdemo 多环境文件配置，开发控制

---
+++ boot_05_maven_and_boot_profile/src/test/java/com/li/Boot02BaseConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot02BaseConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
